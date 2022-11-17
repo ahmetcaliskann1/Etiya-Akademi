78cd3c22042583b56d829ac5613715478aa44209
Ahmet Çalışkan <a.caliskan017@gmail.com>
2022-11-17 09:49:08 +0300

    Pyton-FirstDay

---
+++ main.py
@@ -0,0 +1,53 @@
+print("Merhaba Etiya")
+# string
+text = "Bu bir metindir."
+print(text)
+
+#integer = tam sayı
+number = 10
+print(number)
+#double = ondalıklı sayı
+dnumber = 5.3
+print(dnumber)
+
+#matematiksel operatorler
+print(number -5)
+print(number +5)
+print(number *5)
+print(number /5)
+print(number %5)
+#boolean, bool = true veya false
+isVerified = True
+
+
+#mantıksal operatörler = her mantıksal operator boolean değer döner.
+
+print(1 == 2) 
+
+print( 1 !=2)  
+
+print(2 > 2)
+
+print (2 < 2)
+
+print(2 <= 2)
+
+print(2 >= 2)
+print(50%2==25)
+print(50/2 == 25.0)
+
+#string operasyon
+text = "Merhaba Etiya"
+print(text.upper())
+print(text.lower())
+print(text.startswith("Mer"))
+print(text.endswith("Etiya"))
+
+name = "Ahmet"
+age = 28
+company = "Etiya"
+
+print(f"{name} {age} yaşında {company}' de çalışıyor.")
+
+
+
